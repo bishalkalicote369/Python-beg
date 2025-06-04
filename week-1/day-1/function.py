d88b7560b642f7ac5fa0a9ce91d6762832f6f44f
bishalkalicote369 <bishalkalicotecode@gmail.com>
2025-06-04 14:29:46 +0545

    function

---
+++ week-1/day-1/function.py
@@ -0,0 +1,6 @@
+course = "Python Programming"
+print(len(course))  # lenth of string
+print(course[0])  # indexing
+print(course[-1])  # negative indexing
+print(course[0:4])  # index slice
+print(course[0:])  # all index

2a6cb5c00506d9254d2381788392a14ca0e356d6
bishalkalicote369 <bishalkalicotecode@gmail.com>
2025-06-05 14:09:21 +0545

    string mehods

---
+++ week-1/day-2/format_string.py
@@ -0,0 +1,6 @@
+first_name = "Bishal"
+last_name = "Kalicote"
+full_name = first_name + " " + last_name
+print(full_name)
+full_name1 = f"{len(first_name)} {len(last_name)}"
+print(full_name1)
---
+++ week-1/day-2/string_method.py
@@ -0,0 +1,9 @@
+course = "       bachelor of ai and data science"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.index("ai"))
+print(course.replace("a", "b"))
+print("ai" in course)
+print("ia" in course)

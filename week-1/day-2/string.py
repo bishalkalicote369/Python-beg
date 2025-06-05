7884a55dbbc96c214ebfd681c8e6468a78af52d9
bishalkalicote369 <bishalkalicotecode@gmail.com>
2025-06-05 13:50:02 +0545

    string escape

---
+++ week-1/day-2/string.py
@@ -0,0 +1,5 @@
+message = "hello python"
+print(len(message))
+print(message[0])
+print(message[0:3])
+print(message[:])
---
+++ week-1/escape.py
@@ -0,0 +1,8 @@
+message = "hello \"paris"
+print(message)
+message1 = ("hello \'paris")
+print(message1)
+message2 = ("hello \\ paris")
+print(message2)
+message3 = ("hello \n Paris")
+print(message3)

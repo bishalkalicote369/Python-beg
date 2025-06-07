6747bd5d657b0e9cd14914f1e2eba625b6679f8f
bishalkalicote369 <bishalkalicotecode@gmail.com>
2025-06-07 14:04:58 +0545

    finish

---
+++ week-1/day-4/chain.py
@@ -0,0 +1,19 @@
+# marks = 96
+# if marks >= 70 < 79:
+#     print("B Drade")
+# elif marks >= 80 < 89:
+#     print(" A Grade")
+# elif marks >= 90 < 100:
+#     print("A + Grade")
+# else:
+#     print("not good")
+marks = 96
+
+if 70 <= marks < 80:
+    print("B Grade")
+elif 80 <= marks < 90:
+    print("A Grade")
+elif 90 <= marks <= 100:
+    print("A+ Grade")
+else:
+    print("Not good")
